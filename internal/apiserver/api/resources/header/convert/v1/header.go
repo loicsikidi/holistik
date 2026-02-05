ed317d37423691f6ec373710f4a938338109b132
loicsikidi <loic.sikidi@gmail.com>
2026-02-05 21:20:18 +0100

    progress

--- internal/apiserver/api/resources/header/convert/v1/header.go
+++ internal/apiserver/api/resources/header/convert/v1/header.go
@@ -8,26 +8,6 @@ import (
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-// FromResourceHeaderProto converts the resource header protobuf message into an internal go struct.
-func FromResourceHeaderProto(msg *headerv1.ResourceHeader) header.ResourceHeader {
-	return header.ResourceHeader{
-		Kind:     msg.Kind,
-		SubKind:  msg.SubKind,
-		Version:  msg.Version,
-		Metadata: FromMetadataProto(msg.Metadata),
-	}
-}
-
-// ToResourceHeaderProto converts an internal struct into a v1 resource header protobuf message.
-func ToResourceHeaderProto(resourceHeader header.ResourceHeader) *headerv1.ResourceHeader {
-	return &headerv1.ResourceHeader{
-		Kind:     resourceHeader.Kind,
-		SubKind:  resourceHeader.SubKind,
-		Version:  resourceHeader.Version,
-		Metadata: ToMetadataProto(resourceHeader.Metadata),
-	}
-}
-
 // FromMetadataProto converts v1 metadata into an internal go struct.
 func FromMetadataProto(msg *headerv1.Metadata) header.Metadata {
 	if msg == nil {
